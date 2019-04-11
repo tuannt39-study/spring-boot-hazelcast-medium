fd48fb8ef39dc8ae7eea3ffb1689384a6c193cd7
tuannt39 <tuannt39@gmail.com>
2019-04-11 08:22:39 +0700

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot with Hazelcast
+
+Spring Boot with Hazelcast
+
+- IDE: Intellij
+- https://medium.com/@igorkosandyak/spring-boot-with-hazelcast-b04d13927745
---
+++ src/main/java/vn/sapo/SpringBootHazelcastMediumApplication.java
@@ -0,0 +1,13 @@
+package vn.sapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHazelcastMediumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHazelcastMediumApplication.class, args);
+    }
+
+}
